faeeacc127afb79b109119558caaffe19ed417db
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2023-12-04 18:24:05 -0800

    add feature construction script

---
+++ src/train_rf_model.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import json
+import click
+import numpy as np
+import xarray as xr
+import rioxarray
+from pathlib import Path
+from tqdm import tqdm
+from dask.distributed import Client
+
+
+def get_features(clim, year, feature_info):
+    features = []
+
+    for fbase, finfo in tqdm(list(feature_info.items()), 'Extracting features'):
+        for b in range(finfo['back']):
+            cyear = clim.sel(year=(year - b)).assign_coords(year=[year])
+            for l in range(1, finfo['lags'] + 1):
+                fname = f'{fbase}{l}'
+                new_name = f'{fname}-{b+1}'
+                features.append(
+                    cyear[fname].rename(new_name)
+                )
+
+    return features
+
+
+def to_samples(mort, clim, year, feature_info):
+
+    # Make a new variable to hold copy of the year for each cell
+    year_var = mort['id'].copy().rename('year_copy')
+    year_var[:] = year
+    year_var.attrs['long_name'] = 'year'
+
+    # Merge climate and fold/id features
+    features = [
+        year_var,
+        mort['id'],
+        mort['fold'],
+        mort['tpa'].sel(year=year),
+    ] + get_features(clim, year, feature_info)
+
+    feat_ds = xr.merge(features, join='inner', combine_attrs='drop')
+    feat_ds = feat_ds.drop_vars(('year', 'spatial_ref',))
+    feat_ds = feat_ds.stack(sample=('easting', 'northing'), create_index=False)
+    feat_ds = feat_ds.reset_coords(('northing', 'easting'))
+    feat_ds = feat_ds.rename({ 'year_copy': 'year' })
+
+    return feat_ds
+
+
+@click.command()
+@click.argument('mortalityfile', type=click.Path(
+    path_type=Path, exists=True
+))
+@click.argument('climatefile', type=click.Path(
+    path_type=Path, exists=True
+))
+@click.argument('configfile', type=click.Path(
+    path_type=Path, exists=True
+))
+@click.argument('outputfile', type=click.Path(
+    path_type=Path, exists=False
+))
+def main(mortalityfile, climatefile, configfile, outputfile):
+
+    client = Client()
+    print(f'View progress: {client.dashboard_link}')
+
+    # Load config
+    with open(configfile, 'r') as f:
+        config = json.load(f)
+
+    years = config['years']
+    finfo = config['features']
+    chunks = config['chunks']
+
+    mort = xr.open_zarr(mortalityfile)
+    clim = xr.open_zarr(climatefile)
+
+    # Fix coordinate mis-match issue due to rounding
+    mort = mort.assign_coords(
+        easting=np.round(mort.easting.values, 4),
+        northing=np.round(mort.northing.values, 4),
+    )
+    clim = clim.assign_coords(
+        easting=np.round(clim.easting.values, 4),
+        northing=np.round(clim.northing.values, 4),
+    )
+
+    combined = xr.concat(
+        [
+            to_samples(mort, clim, year, finfo)
+            for year in tqdm(list(range(*years)), 'Yearly Features')
+        ],
+        dim='sample', coords='all', compat='identical'
+    )
+
+    print(f'Selecting data...')
+    good = combined['tpa'].notnull().compute()
+    combined = combined.where(good, drop=True)
+
+    write_job = combined.chunk(chunks).to_zarr(
+        outputfile, mode='w', compute=False, consolidated=True
+    )
+
+    print(f'Writing data...')
+    write_job.compute()
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
