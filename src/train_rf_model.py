78a61e047126da89bc80cac49d05e2cd5a01d2eb
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2023-12-14 16:05:29 -0800

    update training to use LOO

--- src/train_rf_model.py
+++ src/train_rf_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-import json
 import click
 import numpy as np
 import xarray as xr
 from tqdm import tqdm
 from pathlib import Path
-from sklearn.model_selection import GroupShuffleSplit
+from collections import defaultdict
+from sklearn.model_selection import LeaveOneGroupOut
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error as mse, r2_score
 
@@ -24,8 +24,7 @@ def filter_inf(X, y, fill=1e2):
     path_type=Path, exists=False
 ))
 @click.option('-y', '--year', default=2012, type=int)
-@click.option('-s', '--splits', default=5, type=int)
-def main(trainingfile, resultfile, year, splits):
+def main(trainingfile, resultfile, year):
 
     ds = xr.open_zarr(trainingfile)
 
@@ -34,41 +33,40 @@ def main(trainingfile, resultfile, year, splits):
     ds_year = ds.where(idx, drop=True).compute()
 
     groups = ds_year['fold'].to_numpy().astype(int)
+    ids = ds_year['id'].as_numpy()
+    northings = ds_year['northing'].as_numpy()
+    eastings = ds_year['easting'].as_numpy()
     y = ds_year['tpa'].as_numpy()
     X = ds_year.drop_vars(
         ('id', 'fold', 'easting', 'northing', 'year', 'tpa')
     ).to_array().T
 
-    gss = GroupShuffleSplit(n_splits=splits, test_size=0.1, random_state=0)
+    gss = LeaveOneGroupOut()
 
-    rmses = []
-    r2s = []
+    result = defaultdict(list)
 
     iters = enumerate(gss.split(X, y, groups))
-    for i, (trn_idx, tst_idx) in tqdm(list(iters), 'Bootstrapping'):
+    for i, (trn_idx, tst_idx) in tqdm(list(iters), 'Leave-One-Out'):
         Xtrn, ytrn = filter_inf(X[trn_idx], y[trn_idx])
         Xtst, ytst = filter_inf(X[tst_idx], y[tst_idx])
+        result['id'].append(ids[tst_idx])
+        result['northing'].append(northings[tst_idx])
+        result['easting'].append(eastings[tst_idx])
+        result['true'].append(ytst)
 
         rf = RandomForestRegressor(max_depth=5)
         rf.fit(Xtrn, ytrn)
 
         ypred = rf.predict(Xtst)
+        result['pred'].append(ypred)
 
-        rmses.append(mse(ytst, ypred, squared=False))
-        r2s.append(r2_score(ytst, ypred))
-        #print(f'Fold {i} of {splits}:')
-        #print(f'RMSE: {rmse}')
-        #print(f'R^2: {r2}')
+        print(mse(ytst, ypred, squared=False))
 
-    result = {
-        'year': year,
-        'splits': splits,
-        'rmse': rmses,
-        'r2': r2s,
+    result_stacked = {
+        k: np.hstack(v) for k, v in result.items()
     }
 
-    with open(resultfile, 'w') as f:
-        json.dump(result, f, indent=2)
+    np.savez_compressed(resultfile, **result_stacked)
 
 
 if __name__ == '__main__':
