d3e82ff95fcbc6d6c02dff36035f9514614ce17c
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2024-03-13 10:13:30 -0700

    update timeseries plot

--- src/plot_timeseries.py
+++ src/plot_timeseries.py
@@ -6,33 +6,48 @@ import click
 import numpy as np
 import xarray as xr
 import rioxarray
+import geopandas as gpd
 from pathlib import Path
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+from convert_tree_mortality import apply_masks
+
 
 @click.command()
 @click.argument('climatefile', type=click.Path(
     path_type=Path, exists=True
 ))
-@click.argument('datafile', type=click.Path(
+@click.argument('histfile', type=click.Path(
+    path_type=Path, exists=True
+))
+@click.argument('projfile', type=click.Path(
+    path_type=Path, exists=True
+))
+@click.argument('shapefile', type=click.Path(
     path_type=Path, exists=True
 ))
 @click.argument('outputfile', type=click.Path(
     path_type=Path, exists=False
 ))
-def main(climatefile, datafile, outputfile):
+def main(climatefile, histfile, projfile, shapefile, outputfile):
 
-    clim = xr.open_zarr(climatefile)
-    ds = xr.open_zarr(datafile)
+    shape_df = gpd.read_file(shapefile)
+
+    var = 'PR1'
+    var = 'SPI1'
+    clim = xr.open_zarr(climatefile)[var]
+    hist = xr.open_zarr(histfile)
+    ds = xr.open_zarr(projfile)
+
+    clim = apply_masks(clim, {}, shape_df, [])
+    #hist = apply_masks(hist, {}, shape_df, [])
+    ds = apply_masks(ds, {}, shape_df, [])
 
     med = ds.quantile(0.5, dim=('easting', 'northing'), skipna=True)
-    qlo = ds.quantile(0.25, dim=('easting', 'northing'), skipna=True)
-    qhi = ds.quantile(0.75, dim=('easting', 'northing'), skipna=True)
-
-    med_clim = clim.chunk(
+    med_hist = hist.chunk(
         {
             'northing': -1,
             'easting': -1,
@@ -40,30 +55,40 @@ def main(climatefile, datafile, outputfile):
         }
     ).quantile(0.5, dim=('easting', 'northing'), skipna=True)
 
+    med_clim = clim.chunk(
+        {
+            'northing': -1,
+            'easting': -1,
+            'year': 1,
+        }
+    ).mean(dim=('easting', 'northing'), skipna=True)
+    print(med_clim.year.values)
+
     fig = plt.figure(figsize=(16, 6))
     ax = fig.add_subplot(111)
-    ax.fill_between(
-        med.year.values,
-        qlo['tpa_projection_median'].values, qhi['tpa_projection_median'].values,
-        facecolor='gray', edgecolor='none', alpha=0.5
-    )
-    ax.plot(med.year.values, med['tpa_projection_median'].values, 'k-')
-    ax.set_ylim(0, 8)
+
+    ax.plot(med_hist.year.values, med_hist['tpa'].values, 'g-', lw=3, label='Tree Mortality (historical)')
+    ax.plot(med.year.values, med['tpa_projection_median'].values, 'g--', lw=3, label='Tree Mortality (projected)')
+    ax.patch.set_facecolor('none')
+    ax.set_ylim(0, 10)
 
     ax2 = ax.twinx()
-    #ax2.plot(med_clim.year.values, med_clim['SPI2'].values, 'b-', lw=3, label='SPI')
-    ax2.plot(med_clim.year.values + 3, -med_clim['SPI2'].values, 'r-', lw=3, label='SPI')
-    #ax2.plot(med_clim.year.values, med_clim['SPEI2'].values, 'r-', label='SPEI')
-    ax2.set_ylim(-3, 3)
-    #ax2.legend(loc='lower left', ncol=2, fontsize=14)
+    ax2.bar(med_clim.year.values, med_clim.values, facecolor='gray', edgecolor='none', alpha=0.7)
 
-    ax.set_xlim(2025, 2100)
-    ax2.set_xlim(2025, 2100)
+    #ax2.plot(med_clim.year.values, med_clim['SPEI2'].values, 'r-', label='SPEI')
+    ax.legend(loc='upper right', fontsize=14)
+
+    ax.set_xlim(2010, 2100)
+    ax2.set_xlim(2010, 2100)
     ax.set_xlabel('Year', fontsize=16)
-    ax.set_ylabel('Annual Tree Mortality (trees/acre)', fontsize=16)
-    #ax2.set_ylabel('Standardized Precipitation Index (z-score)', fontsize=16, color='b')
-    ax2.set_ylabel('Standardized Precipitation Index (z-score)', fontsize=16, color='r')
-    ax2.tick_params(axis='y', labelcolor='r')
+    ax.set_ylabel('Annual Tree Mortality (trees/acre)', fontsize=16, color='g')
+    if var == 'PR1':
+        ax2.set_ylabel('Annual Precipitation (mm)', fontsize=16)
+    else:
+        ax2.set_ylabel('Standardized Precipitation Index (z-score)', fontsize=16)
+        ax2.set_ylim(-3, 3)
+    ax.tick_params(axis='y', labelcolor='g')
+    ax2.set_zorder(-10)
 
     fig.savefig(outputfile)
 
